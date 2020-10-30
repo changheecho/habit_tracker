0069e60188378fd691d58138ca958a212ca6a583
changhee <changhee@crowdworks.kr>
2020-10-30 17:38:57 +0900

    Updates

---
+++ precache-manifest.5684f249550dfe3c0509c97f6ac5a757.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c2f83c8a340df26786a8afd0626f4e0",
+    "url": "/habit_tracker/index.html"
+  },
+  {
+    "revision": "7f69cdb0b54c764f04bd",
+    "url": "/habit_tracker/static/css/main.9cc1170c.chunk.css"
+  },
+  {
+    "revision": "0a137d472f3af448e4b5",
+    "url": "/habit_tracker/static/js/2.c80a0d7e.chunk.js"
+  },
+  {
+    "revision": "a93ce933847b2bcd16bb222ad89e237b",
+    "url": "/habit_tracker/static/js/2.c80a0d7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f69cdb0b54c764f04bd",
+    "url": "/habit_tracker/static/js/main.c97833be.chunk.js"
+  },
+  {
+    "revision": "dbb297bb1ca874a80b51",
+    "url": "/habit_tracker/static/js/runtime-main.bec78f78.js"
+  }
+]);
